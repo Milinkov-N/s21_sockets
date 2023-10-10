52a8ff487c3de8a0a1230fe063c0b834988697c7
Milinkov Nikita <milinkov.nik@gmail.com>
2023-10-10 10:21:17 +0300

    make library crossplatform

--- include/s21/socket.h
+++ include/s21/socket.h
@@ -1,86 +1,42 @@
 #pragma once
 
+#ifdef _WIN32
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#else
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#endif
 
 #include <cstdint>
 #include <string_view>
 #include <vector>
 
+#define STDCXX_17_OR_HIGHER (__cplusplus >= 201703L || _MSVC_LANG >= 201703L)
+
+#ifndef _WIN32
+typedef int SOCKET;
+
+#define SD_BOTH SHUT_RDWR
+#endif
+
+#ifndef INVALID_SOCKET
+#define INVALID_SOCKET (SOCKET)(~0)
+#endif
+
+#ifndef SOCKET_ERROR
+#define SOCKET_ERROR (SOCKET)(-1)
+#endif
+
+#if _WIN32
+#define errno WSAGetLastError()
+#endif
+
 namespace s21 {
-
-/* Flags that indicate options used in the `getaddrinfo()` function. */
-enum flags {
-  /* The socket address will be used in a call to the `bind()` function. */
-  passive = AI_PASSIVE,
-
-  /* The canonical name is returned in the first `ai_canonname` member. */
-  canonname = AI_CANONNAME,
-
-  /* The nodename parameter passed to the `getaddrinfo()` function must be a
-   * numeric string.
-   */
-  numerichost = AI_NUMERICHOST,
-
-  /* If this bit is set, a request is made for IPv6 addresses and IPv4 addresses
-   * with `AI_V4MAPPED`.
-   */
-  all = AI_ALL,
-
-  /* The `getaddrinfo()` will resolve only if a global address is configured.
-   * The IPv6 and IPv4 loopback address is not considered a valid global
-   * address.
-   */
-  addrconfig = AI_ADDRCONFIG,
-
-  /* If the `getaddrinfo()` request for IPv6 addresses fails, a name service
-   * request is made for IPv4 addresses and these addresses are converted to
-   * IPv4-mapped IPv6 address format.
-   */
-  v4mapped = AI_V4MAPPED,
-
-  /* The address information can be from a non-authoritative namespace provider.
-   * This option is only supported on Windows Vista and later for the `NS_EMAIL`
-   * namespace.
-   */
-  nonauth = AI_NON_AUTHORITATIVE,
-
-  /* The address information is from a secure channel.
-   * This option is only supported on Windows Vista and later for the `NS_EMAIL`
-   * namespace.
-   */
-  secure = AI_SECURE,
-
-  /* The address information is for a preferred name for a user.
-   * This option is only supported on Windows Vista and later for the NS_EMAIL
-   * namespace.
-   */
-  return_preferred_names = AI_RETURN_PREFERRED_NAMES,
-
-  /* If a flat name (single label) is specified, `getaddrinfo()` will return the
-   * fully qualified domain name that the name eventually resolved to. The fully
-   * qualified domain name is returned in the `ai_canonname member`. This is
-   * different than `AI_CANONNAME` bit flag that returns the canonical name
-   * registered in DNS which may be different than the fully qualified domain
-   * name that the flat name resolved to.
-   *
-   * Only one of the `AI_FQDN` and `AI_CANONNAME` bits can be set. The
-   * `getaddrinfo()` function will fail if both flags are present with
-   * `EAI_BADFLAGS`.
-   *
-   * This option is supported on Windows 7, Windows Server 2008 R2, and later.
-   */
-  fqdn = AI_FQDN,
-
-  /* A hint to the namespace provider that the hostname being queried is being
-   * used in a file share scenario. The namespace provider may ignore this hint.
-   * This option is supported on Windows 7, Windows Server 2008 R2, and later.
-   */
-  fileserver = AI_FILESERVER,
-};
-
-/* The address family. Possible values for the address family are defined in the
- * Winsock2.h header file.
+/* The address family. Possible values for the address family are defined in
+ * the Winsock2.h header file.
  */
 enum family : uint16_t {
   /* The address family is unspecified. */
@@ -89,37 +45,42 @@ enum family : uint16_t {
   /* The Internet Protocol version 4 (IPv4) address family. */
   ipv4 = AF_INET,
 
-  /* The NetBIOS address family. This address family is only supported if a
-   * Windows Sockets provider for NetBIOS is installed.
-   */
+/* The NetBIOS address family. This address family is only supported if a
+ * Windows Sockets provider for NetBIOS is installed.
+ */
+#ifdef _WIN32
   netbios = AF_NETBIOS,
+#endif
 
   /* The Internet Protocol version 6 (IPv6) address family. */
   ipv6 = AF_INET6,
 
-  /* The Infrared Data Association (IrDA) address family. This address family is
-   * only supported if the computer has an infrared port and driver installed.
+  /* The Infrared Data Association (IrDA) address family. This address family
+   * is only supported if the computer has an infrared port and driver
+   * installed.
    */
   irda = AF_IRDA,
 
-  /* The Bluetooth address family. This address family is only supported if a
-   * Bluetooth adapter is installed on Windows Server 2003 or later.
-   */
+/* The Bluetooth address family. This address family is only supported if a
+ * Bluetooth adapter is installed on Windows Server 2003 or later.
+ */
+#ifdef _WIN32
   bluetooth = AF_BTH,
+#endif
 };
 
 enum type {
   /* Provides sequenced, reliable, two-way, connection-based byte streams with
-   * an OOB data transmission mechanism. Uses the Transmission Control Protocol
-   * (TCP) for the Internet address family (`AF_INET` or `AF_INET6`). If the
-   * `ai_family` member is `AF_IRDA`, then `SOCK_STREAM` is the only supported
-   * socket type.
+   * an OOB data transmission mechanism. Uses the Transmission Control
+   * Protocol (TCP) for the Internet address family (`AF_INET` or `AF_INET6`).
+   * If the `ai_family` member is `AF_IRDA`, then `SOCK_STREAM` is the only
+   * supported socket type.
    */
   stream = SOCK_STREAM,
 
-  /* Supports datagrams, which are connectionless, unreliable buffers of a fixed
-   * (typically small) maximum length. Uses the User Datagram Protocol (UDP) for
-   * the Internet address family (`AF_INET` or `AF_INET6`).
+  /* Supports datagrams, which are connectionless, unreliable buffers of a
+   * fixed (typically small) maximum length. Uses the User Datagram Protocol
+   * (UDP) for the Internet address family (`AF_INET` or `AF_INET6`).
    */
   datagram = SOCK_DGRAM,
 
@@ -139,9 +100,9 @@ enum type {
 };
 
 enum protocol {
-  /* The Transmission Control Protocol (TCP). This is a possible value when the
-   * `ai_family` member is `AF_INET` or `AF_INET6` and the ai_socktype member is
-   * `SOCK_STREAM`.
+  /* The Transmission Control Protocol (TCP). This is a possible value when
+   * the `ai_family` member is `AF_INET` or `AF_INET6` and the ai_socktype
+   * member is `SOCK_STREAM`.
    */
   tcp = IPPROTO_TCP,
 
@@ -170,12 +131,27 @@ class socket {
   socket& operator=(socket&&) noexcept;
 
  public:
+#if STDCXX_17_OR_HIGHER
   void bind(std::string_view, uint16_t, family = ipv4);
+#else
+  void bind(const char*, uint16_t, family = ipv4);
+#endif
+
+#if STDCXX_17_OR_HIGHER
   void connect(std::string_view, uint16_t, family = ipv4);
+#else
+  void connect(const char*, uint16_t, family = ipv4);
+#endif
+
   void listen(int = SOMAXCONN);
   [[nodiscard]] socket accept();
   std::vector<uint8_t> recieve(size_t);
+
+#if STDCXX_17_OR_HIGHER
   int send(std::string_view);
+#else
+  int send(const std::vector<uint8_t>&);
+#endif
 
  public:
   bool is_valid() const;
--- src/socket.cc
+++ src/socket.cc
@@ -1,14 +1,15 @@
 #include "s21/socket.h"
 
 #include <system_error>
+#include <utility>
 
+#ifdef _WIN32
 static const struct WS2Lib {
  public:
   WS2Lib() {
     WSADATA wsa_data{0};
     int result = WSAStartup(WINSOCK_VERSION, &wsa_data);
-    if (result != 0)
-      throw std::system_error(WSAGetLastError(), std::system_category());
+    if (result != 0) throw std::system_error(errno, std::system_category());
   }
 
   WS2Lib(const WS2Lib&) = delete;
@@ -21,6 +22,7 @@ static const struct WS2Lib {
 
   WS2Lib& operator=(WS2Lib&&) = delete;
 } WS2_;
+#endif
 
 s21::socket::socket() {}
 
@@ -32,7 +34,12 @@ s21::socket::socket(socket&& other) noexcept
 
 s21::socket::~socket() {
   if (connected_) shutdown();
+
+#ifdef _WIN32
   closesocket(sd_);
+#else
+  close(sd_);
+#endif
 }
 
 s21::socket& s21::socket::operator=(socket&& rhs) noexcept {
@@ -40,65 +47,106 @@ s21::socket& s21::socket::operator=(socket&& rhs) noexcept {
   return *this;
 }
 
-void s21::socket::bind(std::string_view name, uint16_t port, family af) {
-  sockaddr_in service{0};
+#if STDCXX_17_OR_HIGHER
+void s21::socket::bind(std::string_view name, uint16_t port, family af)
+#else
+void s21::socket::bind(const char* name, uint16_t port, family af)
+#endif
+{
+#if STDCXX_17_OR_HIGHER
+  auto addr_name = name.data();
+#else
+  if (name == nullptr)
+    throw std::invalid_argument("s21::socket::bind() 'name' was null");
+
+  auto addr_name = name;
+#endif
+
+  sockaddr_in service;
+
+  memset(&service, 0, sizeof(sockaddr_in));
 
   service.sin_family = af;
-  inet_pton(af, name.data(), &service.sin_addr.s_addr);
+  inet_pton(af, addr_name, &service.sin_addr.s_addr);
   service.sin_port = port;
 
   int res =
       ::bind(sd_, reinterpret_cast<const sockaddr*>(&service), sizeof(service));
   if (res == SOCKET_ERROR)
-    throw std::system_error(WSAGetLastError(), std::system_category());
+    throw std::system_error(errno, std::system_category());
 }
 
-void s21::socket::connect(std::string_view name, uint16_t port, family af) {
-  sockaddr_in service{0};
+#if STDCXX_17_OR_HIGHER
+void s21::socket::connect(std::string_view name, uint16_t port, family af)
+#else
+void s21::socket::connect(const char* name, uint16_t port, family af)
+#endif
+{
+#if STDCXX_17_OR_HIGHER
+  auto addr_name = name.data();
+#else
+  if (name == nullptr)
+    throw std::invalid_argument("s21::socket::connect() 'name' was null");
+
+  auto addr_name = name;
+#endif
+
+  sockaddr_in service;
+
+  memset(&service, 0, sizeof(sockaddr_in));
 
   service.sin_family = af;
-  inet_pton(af, name.data(), &service.sin_addr.s_addr);
+  inet_pton(af, addr_name, &service.sin_addr.s_addr);
   service.sin_port = port;
 
   int res = ::connect(sd_, reinterpret_cast<const sockaddr*>(&service),
                       sizeof(service));
   if (res == SOCKET_ERROR)
-    throw std::system_error(WSAGetLastError(), std::system_category());
+    throw std::system_error(errno, std::system_category());
 }
 
 void s21::socket::listen(int backlog) {
+#ifdef _WIN32
   int res =
       ::listen(sd_, (backlog == SOMAXCONN ? backlog : SOMAXCONN_HINT(backlog)));
+#else
+  int res = ::listen(sd_, backlog);
+#endif
   if (res == SOCKET_ERROR)
-    throw std::system_error(WSAGetLastError(), std::system_category());
+    throw std::system_error(errno, std::system_category());
 }
 
 s21::socket s21::socket::accept() {
   SOCKET client = ::accept(sd_, nullptr, nullptr);
   if (client == SOCKET_ERROR)
-    throw std::system_error(WSAGetLastError(), std::system_category());
+    throw std::system_error(errno, std::system_category());
   return socket(client);
 }
 
 std::vector<uint8_t> s21::socket::recieve(size_t bufsz) {
   std::vector<uint8_t> buf(bufsz);
   int bytes = ::recv(sd_, (char*)buf.data(), (int)buf.size(), 0);
-  if (bytes == -1)
-    throw std::system_error(WSAGetLastError(), std::system_category());
+  if (bytes == -1) throw std::system_error(errno, std::system_category());
   buf.resize(bytes);
   connected_ = true;
   return buf;
 }
 
-int s21::socket::send(std::string_view buf) {
+#if STDCXX_17_OR_HIGHER
+int s21::socket::send(std::string_view buf)
+#else
+int s21::socket::send(const std::vector<uint8_t>& buf)
+#endif
+{
   connected_ = true;
-  return ::send(sd_, buf.data(), (int)buf.size(), 0);
+  return ::send(sd_, reinterpret_cast<const char*>(buf.data()), (int)buf.size(),
+                0);
 }
 
 void s21::socket::shutdown() {
   int result = ::shutdown(sd_, SD_BOTH);
   if (result == SOCKET_ERROR)
-    throw std::system_error(WSAGetLastError(), std::system_category());
+    throw std::system_error(errno, std::system_category());
 }
 
 bool s21::socket::is_valid() const { return sd_ != INVALID_SOCKET; }
